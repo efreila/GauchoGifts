5eb6b9e5615ee78eb03e4836792850808c783964
Eric Freilafert <efreilafert@gmail.com>
2017-02-10 13:45:30 -0800

    First commit

---
+++ app/src/main/java/com/cs48/g12/gauchogifts/intro.java
@@ -0,0 +1,26 @@
+package com.cs48.g12.gauchogifts;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class intro extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        Button login = (Button)findViewById(R.id.logIn);
+        login.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), login.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/cs48/g12/gauchogifts/login.java
@@ -0,0 +1,50 @@
+package com.cs48.g12.gauchogifts;
+
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class login extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseAuth.AuthStateListener mAuthListener;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user == null)
+                {
+
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    public void busignin(View view) {
+
+    }
+}
