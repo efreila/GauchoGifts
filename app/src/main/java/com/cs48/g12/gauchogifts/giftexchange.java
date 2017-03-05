6ab5eb0638fb93e592f4f1c2d7d7c2b031695e62
Eric Freilafert <efreilafert@gmail.com>
2017-03-04 16:06:39 -0800

    Added database writing for joining exchanges

--- app/src/main/java/com/cs48/g12/gauchogifts/giftexchange.java
+++ app/src/main/java/com/cs48/g12/gauchogifts/giftexchange.java
@@ -11,12 +11,19 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 
 public class giftexchange extends AppCompatActivity {
 
     private TextView acTitle;
     private TextView acDeadline;
     private TextView acDescription;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,8 @@ public class giftexchange extends AppCompatActivity {
         acTitle = (TextView)findViewById(R.id.geTitle);
         acDeadline = (TextView)findViewById(R.id.geDeadline);
         acDescription = (TextView)findViewById(R.id.geDescripton);
+        mAuth = FirebaseAuth.getInstance();
+
 
         Bundle geinfo = getIntent().getExtras();
 
@@ -43,12 +52,57 @@ public class giftexchange extends AppCompatActivity {
         Button join = (Button)findViewById(R.id.joinBtn);
         join.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
-                Intent myIntent = new Intent(view.getContext(), navheader.class);
-                startActivityForResult(myIntent, 0);
+                joinGiftExchange();
             }
         });
 
-    };
+//        mAuthListener = new FirebaseAuth.AuthStateListener() {
+//            @Override
+//            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+//                FirebaseUser user = firebaseAuth.getCurrentUser();
+//                if (user != null) {
+//                    // User is signed in
+//                    // NOTE: this Activity should get open only when the user is not signed in, otherwise
+//                    // the user will receive another verification email.
+//                } else {
+//                    // User is signed out
+//                }
+//                // ...
+//            }
+//        };
+
+    }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        mAuth.addAuthStateListener(mAuthListener);
+//    }
+//
+//    @Override
+//    public void onStop() {
+//        super.onStop();
+//        if (mAuthListener != null) {
+//            mAuth.removeAuthStateListener(mAuthListener);
+//        }
+//    }
+
+    private void joinGiftExchange() {
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference();
+        Bundle geinfo = getIntent().getExtras();
+        String exchangeTitle = geinfo.getString("Title");
+
+        myRef.child("Exchanges").child(exchangeTitle).child("Enrolled Users").child(mAuth.getCurrentUser().getUid()).child("Questions").setValue("Hello");
+        myRef.child("Users").child(mAuth.getCurrentUser().getUid()).child("Current Exchanges").child(exchangeTitle).setValue(exchangeTitle);
+
+
+        Intent myIntent = new Intent(giftexchange.this, navheader.class);
+        startActivity(myIntent);
+    }
+
+
 
     @Override
     public void onBackPressed() {
